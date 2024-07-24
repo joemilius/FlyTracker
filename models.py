271f501a3dd17fe91a961169d88272102c233700
Joe Milius <joemilius@gmail.com>
2024-07-24 13:40:53 -0600

    created Fisherperson model

--- models.py
+++ models.py
@@ -8,4 +8,8 @@ from config import db
 class Fisherperson(db.Model, SerializerMixin):
     __tablename__ = 'fisherpersons'
 
-    id = 
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.Sting)
+    lastname = db.Column(db.String)
+    username = db.Column(db.String)
+    state = db.Column(db.String)
